9988d1b247e342d07f51fc0d56b9e6a6484d69e4
leoMarkDev <leomarkpaway@gmail.com>
2023-05-19 13:10:59 +0800

    :sparkles: Add use case on module

--- app/src/main/java/com/leomarkpaway/autocalculateworkingdaysdatepicker/di/UseCaseModule.kt
+++ app/src/main/java/com/leomarkpaway/autocalculateworkingdaysdatepicker/di/UseCaseModule.kt
@@ -1,10 +1,18 @@
 package com.leomarkpaway.numbertowordconverter.di
 
+import com.leomarkpaway.autocalculateworkingdaysdatepicker.common.util.DatePickerHelper
+import com.leomarkpaway.autocalculateworkingdaysdatepicker.domain.usecase.CalculateWorkingDaysUseCase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideCalculateWorkingDaysUseCase(datePickerHelper: DatePickerHelper) = CalculateWorkingDaysUseCase(datePickerHelper)
 }
