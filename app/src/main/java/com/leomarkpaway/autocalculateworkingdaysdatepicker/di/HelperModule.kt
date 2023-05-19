470c8f812a0df96c8ceffacdf6d72d2228400f76
leoMarkDev <leomarkpaway@gmail.com>
2023-05-19 13:08:38 +0800

    :sparkles: Add module date picker

--- app/src/main/java/com/leomarkpaway/autocalculateworkingdaysdatepicker/di/HelperModule.kt
+++ app/src/main/java/com/leomarkpaway/autocalculateworkingdaysdatepicker/di/HelperModule.kt
@@ -1,10 +1,18 @@
 package com.leomarkpaway.numbertowordconverter.di
 
+import com.leomarkpaway.autocalculateworkingdaysdatepicker.common.util.DatePickerHelper
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class HelperModule {
+
+    @Provides
+    @Singleton
+    fun provideDatePickHelper() = DatePickerHelper
+
 }
