e70cf05b3bdbef2d09d2e6942ed47488e53bc10d
leoMarkDev <leomarkpaway@gmail.com>
2023-05-19 13:09:17 +0800

    :sparkles: Create collect latest date extension function

---
+++ app/src/main/java/com/leomarkpaway/autocalculateworkingdaysdatepicker/common/util/ColectLatestDataExt.kt
@@ -0,0 +1,13 @@
+package com.leomarkpaway.autocalculateworkingdaysdatepicker.common.util
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T> StateFlow<T>.collectLatestData(scope: CoroutineScope, action: (T) -> Unit): Job {
+    return scope.launch {
+        this@collectLatestData.collectLatest { data -> action(data) }
+    }
+}
